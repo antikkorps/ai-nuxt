d260c93128d84e3ac675588dec123f75703674d3
Franck <franckvienot7@gmail.com>
2024-05-15 21:06:54 +0200

    make some visual adjustments

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,13 +3,15 @@
 </script>
 <template>
   <div class="flex flex-row fixed w-full items-center justify-between px-6 py-4 bg-white dark:bg-neutral-800">
+    <div class="flex flex-row items-center justify-between space-x-3">
     <div class="text-2xl dark:text-white">logo</div>
-    <div class="list-items">
-      <div class="flex flex-row items-center justify-between dark:text-white space-x-2">
+    <div class="list-items pl-4">
+      <div class="flex flex-row items-center justify-between dark:text-white space-x-6">
         <div><NuxtLink to="/">Item1</NuxtLink></div>
         <div><NuxtLink to="/">Item2</NuxtLink></div>
         <div><NuxtLink to="/">Item3</NuxtLink></div>
       </div>
+    </div>
     </div>
     <div class="flex items-center justify-between dark:text-white space-x-2">
       <div>Some Ui</div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex flex-col h-screen items-center bg-gray-100">
-    <h1 class="text-3xl w-full text-center py-16">This is the index page</h1>
+  <div class="flex flex-col h-screen items-center bg-gray-100 dark:bg-neutral-800">
+    <h1 class="text-3xl w-full text-center dark:text-white py-16">This is the index page</h1>
   </div>
 </template>
 <script setup lang="ts">
