7fd4cedf7108f0552121cfc6596d7ae288d3b7e3
Franck <franckvienot7@gmail.com>
2024-05-23 16:56:10 +0200

    authentication ok

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from "node:path"
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
@@ -5,14 +6,17 @@ export default defineNuxtConfig({
   colorMode: {
     preference: 'dark'
   },
+  alias: {
+    cookie: resolve(__dirname, "node_modules/cookie")
+  },
   runtimeConfig: {
     authJs: {
       secret: process.env.NUXT_NEXTAUTH_SECRET // You can generate one with `openssl rand -base64 32`
     },
-    github: {
-      clientId: process.env.NUXT_GITHUB_CLIENT_ID,
-      clientSecret: process.env.NUXT_GITHUB_CLIENT_SECRET
-    },
+    // github: {
+    //   clientId: process.env.NUXT_GITHUB_CLIENT_ID,
+    //   clientSecret: process.env.NUXT_GITHUB_CLIENT_SECRET,
+    // },
     public: {
       authJs: {
         baseUrl: process.env.NUXT_NEXTAUTH_URL, // The URL of your deployed app (used for origin Check in production)
